0c1a867da84a2eae0cdc44e8e139742dafd05cc6
Rui Vieira <ruidevieira@googlemail.com>
2022-10-15 21:43:08 +0100

    Add list user repos

--- src/gitea.nim
+++ src/gitea.nim
@@ -39,11 +39,75 @@ type GiteaLabel* = object
   description*: string
   url*: string
 
-type GiteaRepository* = object
+type GiteaSimpleRepository* = object
   id*: int
   name*: string
   owner*: string
   `full_name`*: string
+
+type GiteaExternalWiki* = object
+  external_wiki_url*: string
+
+type GiteaExternalTracker* = object
+  external_tracker_format*: string
+  external_tracker_style*: string
+  external_tracker_url*: string
+
+type GiteaInternalTracker* = object
+  allow_only_contributors_to_track_time*: bool
+  enable_issue_dependencies*: bool
+  enable_time_tracker*: bool
+
+type GiteaPermission* = object
+  admin*: bool
+  pull*: bool
+  push*: bool
+
+type GiteaRepository* = object
+  `allow_merge_commits*`: bool
+  `allow_rebase*`: bool
+  `allow_rebase_explicit*`: bool
+  `allow_squash_merge*`: bool
+  archived*: bool
+  avatar_url*: string
+  clone_url*: string
+  created_at*: string
+  default_branch*: string
+  default_merge_style*: string
+  description*: string
+  empty*: bool
+  external_tracker*: GiteaExternalTracker
+  external_wiki*: GiteaExternalWiki
+  fork*: bool
+  forks_count*: int
+  full_name*: string
+  has_issues*: bool
+  has_projects*: bool
+  has_pull_requests*: bool
+  has_wiki*: bool
+  html_url*: string
+  id*: int
+  ignore_whitespace_conflicts*: bool
+  internal*: bool
+  internal_tracker*: GiteaInternalTracker
+  mirror*: bool
+  mirror_interval*: string
+  name*: string
+  open_issues_count*: int
+  open_pr_counter*: int
+  original_url*: string
+  owner*: GiteaUser
+  # parent	{...}
+  permissions*: GiteaPermission
+  private*: bool
+  release_counter*: int
+  size*: int
+  ssh_url*: string
+  stars_count*: int
+  `template`*: bool
+  updated_at*: string
+  watchers_count*: int
+  website*: string
 
 type GiteaIssue* = object
   id*: int
@@ -68,7 +132,7 @@ type GiteaIssue* = object
   `closed_at`*: string
   `due_date`*: string
   `pull_request`*: string
-  repository*: GiteaRepository
+  repository*: GiteaSimpleRepository
 
 type GiteaCreateIssue* = object
   assignees*: Option[seq[GiteaUser]]
@@ -169,3 +233,8 @@ proc getAllIssues*(gitea: Gitea, state: string): seq[GiteaIssue] =
           "&limit=" & $limit
   let issues = getAll[GiteaIssue](gitea, url)
   return issues
+
+proc getUserRepos*(gitea: Gitea): seq[GiteaRepository] =
+  let url = gitea.host & "/user/repos"
+  let repos = getAll[GiteaRepository](gitea, url)
+  return repos
